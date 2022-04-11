9349dd09e38ba36e9c5942b52961d86fa25d44a3
Mehdi Salimi <Salimi.Devop@gmail.com>
2022-04-11 23:55:38 +0430

    style:HeaderContent style added

--- src/components/HeaderContent.js
+++ src/components/HeaderContent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from '../styles/layout/HeaderContent.module.scss';
 import logo from '../assets/logo/logo-small.png'
+import {ArrowDropDown} from '@mui/icons-material';
 
 const HeaderContent = () => {
     return (
@@ -13,10 +14,10 @@ const HeaderContent = () => {
                 </span>
                 <nav className={styles.header_menu}>
                     <ul>
-                        <li> <a href="/#">Home</a> </li>
-                        <li> <a href="/#">Rooms</a> </li>
-                        <li> <a href="/#">Restaurant</a> </li>
-                        <li> <a href="/#">Pages</a> </li>
+                        <li> <a href="/#">Home </a> </li>
+                        <li> <a href="/#">Rooms <span><ArrowDropDown/></span></a> </li>
+                        <li> <a href="/#">Restaurant <span><ArrowDropDown/></span></a> </li>
+                        <li> <a href="/#">Pages<span><ArrowDropDown/></span> </a> </li>
                         <li> <a href="/#">Galleries</a> </li>
                         <li> <a href="/#">Blog</a> </li>
                         <li> <a href="/#">Contact</a> </li>
