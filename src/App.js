4fe2021864e4354d56adc0061fdccce4f24159fe
Mehdi Salimi <Salimi.Devop@gmail.com>
2022-04-12 21:05:06 +0430

    Started to create home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 //Componennts
 import Header from './components/Header';
+import Home from './pages/Home';
 
 function App() {
     return (
         <>
             <Header />
+            <Home />
         </>
     );
 }
