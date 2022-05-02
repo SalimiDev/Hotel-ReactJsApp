11b86b5c501bc970ed2c5403d08117ee21663a04
Mehdi Salimi <Salimi.Devop@gmail.com>
2022-05-02 20:12:00 +0430

    style:Add typography styles (_typography.scss) to body

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/global.scss';
 import App from './App';
+import './styles/utils/_typography.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
