244355a22277d33f4c068a9a617d7955d6eb3ebd
Mehdi Salimi <Salimi.Devop@gmail.com>
2022-05-09 18:21:19 +0430

    feat:Add check availability form section

---
+++ src/components/AvailabilityForm.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import styles from '../styles/layout/AvailabilityForm.module.scss';
+import { CheckRounded } from '@mui/icons-material';
+
+const AvailabilityForm = () => {
+    //Get current date
+    const currentDate = (date, day) => {
+        const month = new Intl.DateTimeFormat('en-US', { month: 'long' }).format(date);
+        const formatMap = {
+            mm: month,
+            dd: date.getDate() + day,
+            yyyy: date.getFullYear(),
+        };
+        const { mm, dd, yyyy } = formatMap;
+        const dateFormat = `${mm} ${dd} , ${yyyy}`;
+        return dateFormat;
+    };
+
+    return (
+        <section>
+            <div className={styles.container}>
+                <div className={styles.check_availability_title}>
+                    <span className={styles.check_icon}>
+                        <CheckRounded />
+                    </span>
+                    <h2>Check Availability</h2>
+                </div>
+                <form action='submit' className={styles.availability_form}>
+                    <div className={styles.date_container}>
+                        <input type='text' defaultValue={currentDate(new Date(), 0)} />
+                        <input type='text' defaultValue={currentDate(new Date(), 1)} />
+                    </div>
+                    <div className={styles.capacity_container}>
+                        <select>
+                            <option value='Adults'>Adults</option>
+                            <option value='1'>1</option>
+                            <option value='2'>2</option>
+                            <option value='3'>3</option>
+                            <option value='4'>4</option>
+                            <option value='5'>5</option>
+                            <option value='6'>6</option>
+                        </select>
+                        <select>
+                            <option value='children'>Children</option>
+                            <option value='1'>1</option>
+                            <option value='2'>2</option>
+                            <option value='3'>3</option>
+                            <option value='4'>4</option>
+                            <option value='5'>5</option>
+                            <option value='6'>6</option>
+                        </select>
+                    </div>
+                    <button type='submit' className='btn btn-lg btn-orange'>
+                        check Availability
+                    </button>
+                </form>
+            </div>
+        </section>
+    );
+};
+
+export default AvailabilityForm;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 //Components
 import Slider from '../components/Slider';
+import AvailabilityForm from '../components/AvailabilityForm';
 
 const Home = () => {
     return (
         <div>
-            <Slider/>
+            <Slider />
+            <AvailabilityForm />
         </div>
     );
 };
