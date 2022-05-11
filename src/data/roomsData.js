4a267fe2ec059f003d6f11e182759b0b4cef746e
Mehdi Salimi <Salimi.Devop@gmail.com>
2022-05-11 23:33:56 +0430

    build:Add rooms data and images

---
+++ src/data/roomsData.js
@@ -0,0 +1,88 @@
+//Rooms Image
+import room1 from '../assets/images/room-1.jpg';
+import room2 from '../assets/images/room-2.jpg';
+import room3 from '../assets/images/room-3.jpg';
+import room4 from '../assets/images/room-4.jpg';
+import room5 from '../assets/images/room-5.jpg';
+import room6 from '../assets/images/room-6.jpg';
+
+export const roomData = [
+    {
+        id: 1,
+        title: 'Couple Room',
+        price: 160,
+        image: room1,
+        shortDescrip:
+            'Nullam nec bibendum libero, eget ultricies urna. Ut fermentum ligula dui, sodales accumsan ante ornare nec. Vivamus convallis, velit tincidunt mollis scelerisque, mauris lorem porttitor sem, at molestie nibh enim ut tellus',
+        bodyDescrip1:
+            'Located in the heart of Aspen with a unique blend of contemporary luxury and historic heritage, deluxe accommodations, superb amenities, genuine hospitality and dedicated service for an elevated experience in the Rocky Mountains.',
+        bodyDescrip2:
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.',
+        Amenities: ['Allowed Pets', 'Free Wifi', 'Television'],
+    },
+    {
+        id: 2,
+        title: 'Single Room',
+        price: 80,
+        image: room2,
+        shortDescrip:
+            'Nullam nec bibendum libero, eget ultricies urna. Ut fermentum ligula dui, sodales accumsan ante ornare nec. Vivamus convallis, velit tincidunt mollis scelerisque, mauris lorem porttitor sem, at molestie nibh enim ut tellus',
+        bodyDescrip1:
+            'Cras ut turpis neque. Duis dapibus massa justo, eget porta urna ultrices sodales. In in nisi ornare, pharetra tortor sed, pellentesque mauris. Morbi sed mattis purus, vel fringilla nisl. Nullam at justo a arcu ullamcorper lacinia a id enim. Praesent mi purus, feugiat a aliquet tempor, auctor at est. Aenean ligula felis, vulputate et dolor eu, lacinia sollicitudin neque. Donec blandit tincidunt mauris, ut convallis nulla venenatis quis. Etiam gravida velit non urna porttitor ullamcorper.',
+        bodyDescrip2:
+            'Nullam nec bibendum libero, eget ultricies urna. Ut fermentum ligula dui, sodales accumsan ante ornare nec. Vivamus convallis, velit tincidunt mollis scelerisque, mauris lorem porttitor sem, at molestie nibh enim ut tellus. Aenean elementum leo ac metus vestibulum luctus. In pharetra lectus urna, id hendrerit velit rhoncus eu. Vivamus mollis condimentum neque, non feugiat felis tincidunt vitae. Aenean non nisl et est cursus volutpat.',
+        Amenities: ['Allowed Pets', 'Free Wifi', 'Television'],
+    },
+    {
+        id: 3,
+        title: 'Deluxe Room',
+        price: 200,
+        image: room3,
+        shortDescrip:
+            'Nullam nec bibendum libero, eget ultricies urna. Ut fermentum ligula dui, sodales accumsan ante ornare nec. Vivamus convallis, velit tincidunt mollis scelerisque, mauris lorem porttitor sem, at molestie nibh enim ut tellus',
+        bodyDescrip1:
+            'Cras ut turpis neque. Duis dapibus massa justo, eget porta urna ultrices sodales. In in nisi ornare, pharetra tortor sed, pellentesque mauris. Morbi sed mattis purus, vel fringilla nisl. Nullam at justo a arcu ullamcorper lacinia a id enim. Praesent mi purus, feugiat a aliquet tempor, auctor at est. Aenean ligula felis, vulputate et dolor eu, lacinia sollicitudin neque. Donec blandit tincidunt mauris, ut convallis nulla venenatis quis. Etiam gravida velit non urna porttitor ullamcorper.',
+        bodyDescrip2:
+            'Nullam nec bibendum libero, eget ultricies urna. Ut fermentum ligula dui, sodales accumsan ante ornare nec. Vivamus convallis, velit tincidunt mollis scelerisque, mauris lorem porttitor sem, at molestie nibh enim ut tellus. Aenean elementum leo ac metus vestibulum luctus. In pharetra lectus urna, id hendrerit velit rhoncus eu. Vivamus mollis condimentum neque, non feugiat felis tincidunt vitae. Aenean non nisl et est cursus volutpat.',
+        Amenities: ['Allowed Pets', 'Free Wifi', 'Television'],
+    },
+    {
+        id: 4,
+        title: 'Standard Room',
+        price: 150,
+        image: room4,
+        shortDescrip:
+            'Nullam nec bibendum libero, eget ultricies urna. Ut fermentum ligula dui, sodales accumsan ante ornare nec. Vivamus convallis, velit tincidunt mollis scelerisque, mauris lorem porttitor sem, at molestie nibh enim ut tellus',
+        bodyDescrip1:
+            'Cras ut turpis neque. Duis dapibus massa justo, eget porta urna ultrices sodales. In in nisi ornare, pharetra tortor sed, pellentesque mauris. Morbi sed mattis purus, vel fringilla nisl. Nullam at justo a arcu ullamcorper lacinia a id enim. Praesent mi purus, feugiat a aliquet tempor, auctor at est. Aenean ligula felis, vulputate et dolor eu, lacinia sollicitudin neque. Donec blandit tincidunt mauris, ut convallis nulla venenatis quis. Etiam gravida velit non urna porttitor ullamcorper.',
+        bodyDescrip2:
+            'Nullam nec bibendum libero, eget ultricies urna. Ut fermentum ligula dui, sodales accumsan ante ornare nec. Vivamus convallis, velit tincidunt mollis scelerisque, mauris lorem porttitor sem, at molestie nibh enim ut tellus. Aenean elementum leo ac metus vestibulum luctus. In pharetra lectus urna, id hendrerit velit rhoncus eu. Vivamus mollis condimentum neque, non feugiat felis tincidunt vitae. Aenean non nisl et est cursus volutpat.',
+        Amenities: ['Allowed Pets', 'Free Wifi', 'Television'],
+    },
+    {
+        id: 5,
+        title: 'Angle Room',
+        price: 170,
+        image: room5,
+        shortDescrip:
+            'Nullam nec bibendum libero, eget ultricies urna. Ut fermentum ligula dui, sodales accumsan ante ornare nec. Vivamus convallis, velit tincidunt mollis scelerisque, mauris lorem porttitor sem, at molestie nibh enim ut tellus',
+        bodyDescrip1:
+            'Cras ut turpis neque. Duis dapibus massa justo, eget porta urna ultrices sodales. In in nisi ornare, pharetra tortor sed, pellentesque mauris. Morbi sed mattis purus, vel fringilla nisl. Nullam at justo a arcu ullamcorper lacinia a id enim. Praesent mi purus, feugiat a aliquet tempor, auctor at est. Aenean ligula felis, vulputate et dolor eu, lacinia sollicitudin neque. Donec blandit tincidunt mauris, ut convallis nulla venenatis quis. Etiam gravida velit non urna porttitor ullamcorper.',
+        bodyDescrip2:
+            'Nullam nec bibendum libero, eget ultricies urna. Ut fermentum ligula dui, sodales accumsan ante ornare nec. Vivamus convallis, velit tincidunt mollis scelerisque, mauris lorem porttitor sem, at molestie nibh enim ut tellus. Aenean elementum leo ac metus vestibulum luctus. In pharetra lectus urna, id hendrerit velit rhoncus eu. Vivamus mollis condimentum neque, non feugiat felis tincidunt vitae. Aenean non nisl et est cursus volutpat.',
+        Amenities: ['Allowed Pets', 'Free Wifi', 'Television'],
+    },
+    {
+        id: 6,
+        title: 'Prince Room',
+        price: 190,
+        image: room6,
+        shortDescrip:
+            'Nullam nec bibendum libero, eget ultricies urna. Ut fermentum ligula dui, sodales accumsan ante ornare nec. Vivamus convallis, velit tincidunt mollis scelerisque, mauris lorem porttitor sem, at molestie nibh enim ut tellus',
+        bodyDescrip1:
+            'Cras ut turpis neque. Duis dapibus massa justo, eget porta urna ultrices sodales. In in nisi ornare, pharetra tortor sed, pellentesque mauris. Morbi sed mattis purus, vel fringilla nisl. Nullam at justo a arcu ullamcorper lacinia a id enim. Praesent mi purus, feugiat a aliquet tempor, auctor at est. Aenean ligula felis, vulputate et dolor eu, lacinia sollicitudin neque. Donec blandit tincidunt mauris, ut convallis nulla venenatis quis. Etiam gravida velit non urna porttitor ullamcorper.',
+        bodyDescrip2:
+            'Nullam nec bibendum libero, eget ultricies urna. Ut fermentum ligula dui, sodales accumsan ante ornare nec. Vivamus convallis, velit tincidunt mollis scelerisque, mauris lorem porttitor sem, at molestie nibh enim ut tellus. Aenean elementum leo ac metus vestibulum luctus. In pharetra lectus urna, id hendrerit velit rhoncus eu. Vivamus mollis condimentum neque, non feugiat felis tincidunt vitae. Aenean non nisl et est cursus volutpat.',
+        Amenities: ['Allowed Pets', 'Free Wifi', 'Television'],
+    },
+];
