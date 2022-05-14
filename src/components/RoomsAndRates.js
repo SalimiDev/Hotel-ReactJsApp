b46726060b87574efa7df8fe446bbd7cccfab7ec
Mehdi Salimi <Salimi.Devop@gmail.com>
2022-05-14 05:50:36 +0430

    style:Add responsiveness design to RoomsAndRates component *Edited and refactored some UI codes

--- src/components/RoomsAndRates.js
+++ src/components/RoomsAndRates.js
@@ -4,23 +4,25 @@ import { roomsData } from '../data/roomsData';
 
 const RoomsAndRates = () => {
     return (
-        <section className={styles.container}>
-            <div className={styles.textContainer}>
-                <h2>ROOMS & RATES</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing</p>
-            </div>
-            <div className={styles.roomsContainer}>
-                {roomsData.slice(0, 4).map(room => (
-                    <div className={styles.roomItems}>
-                        <img src={`${room.image}`} alt={`${room.title}`} />
-                        <div className={styles.lable}>
-                            <h2>{room.title}</h2>
-                            <p>
-                                <span>${room.price}</span>/days
-                            </p>
+        <section className={styles.roomAndrates}>
+            <div className={styles.container}>
+                <div className={styles.textContainer}>
+                    <h2>ROOMS & RATES</h2>
+                    <p>Lorem Ipsum is simply dummy text of the printing</p>
+                </div>
+                <div className={styles.roomsContainer}>
+                    {roomsData.slice(0, 4).map(room => (
+                        <div className={styles.roomItems}>
+                            <img src={`${room.image}`} alt={`${room.title}`} />
+                            <div className={styles.lable}>
+                                <h2>{room.title}</h2>
+                                <p>
+                                    <span>${room.price}</span>/days
+                                </p>
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
         </section>
     );
