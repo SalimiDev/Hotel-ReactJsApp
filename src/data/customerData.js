6e0cca51ff217c1cbdad0c87b6edf8664266b659
Mehdi Salimi <Salimi.Devop@gmail.com>
2022-05-18 00:05:48 +0430

    build:Add customers(guest) data

---
+++ src/data/customerData.js
@@ -0,0 +1,31 @@
+//Customers picture
+import customer1 from '../assets/images/customer-1.jpg';
+import customer2 from '../assets/images/customer-2.jpg';
+import customer3 from '../assets/images/customer-3.jpg';
+
+export const customerData = [
+    {
+        id: 1,
+        fullName: 'Amie Bublitz',
+        city: 'Kokomo',
+        picture: customer1,
+        comment:
+            'Donec nec enim turpis. Quisque consequat ipsum libero, et rhoncus augue condimentum a. Integer blandit libero in est interdum vestibulum. In dictum posuere pharetra',
+    },
+    {
+        id: 2,
+        fullName: 'Donna Ginter',
+        city: ' Marietta',
+        picture: customer2,
+        comment:
+            'This is the only place to stay in Catalina! I have stayed in the cheaper hotels and they were fine, but this is just the icing on the cake! After spending the day bike riding and hiking to come back and enjoy a glass of wine while looking out your ocean view window and then to top it all off...',
+    },
+    {
+        id: 3,
+        fullName: 'Rosalind Cloer',
+        city: 'Oxford',
+        picture: customer3,
+        comment:
+            'Praesent fringilla convallis libero, vel ullamcorper justo luctus sed. Sed dapibus eu mi ut convallis. Pellentesque egestas magna ac eros tempus sollicitudin. Aenean eget rhoncus ante.',
+    },
+];
