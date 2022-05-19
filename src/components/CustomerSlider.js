006672c514278135dc95825627db46afa5c5ba32
Mehdi Salimi <Salimi.Devop@gmail.com>
2022-05-19 23:39:25 +0430

    feat:Add functionality to customer slider(banner)

--- src/components/CustomerSlider.js
+++ src/components/CustomerSlider.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from '../styles/layout/CustomerSlider.module.scss';
 import { customerData } from '../data/customerData';
 
 const CustomerSlider = () => {
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const slideNumber = 'slide_' + currentSlide;
+
+    //variables for create auto scrolling slider
+    const autoScroll = true;
+    let slideInterval;
+    let intervalTime = 3500;
+
+    //slider length is equal to 3
+    const slideLength = customerData.length;
+
+    const nextSlide = () => {
+        setCurrentSlide(currentSlide === slideLength - 1 ? 0 : currentSlide + 1);
+    };
+
+    //functionality of auto scroll slider
+    const auto = () => {
+        slideInterval = setInterval(nextSlide, intervalTime);
+    };
+    useEffect(() => {
+        if (autoScroll) {
+            auto();
+        }
+        return () => clearInterval(slideInterval);
+    }, [currentSlide]);
+
     return (
         <section className={styles.customer_slider}>
             <div className={styles.container}>
-                <div className={styles.customer_wrapper}>
-                    {customerData.map(data => (
-                        <div className={styles.customer_data} key={data.id}>
+                <div className={styles.items_wrapper}>
+                    {customerData.map((data, index) => (
+                        <div
+                            id={`${ styles[slideNumber]}`}
+                            className={styles.items}
+                            key={data.id}>
                             <img src={data.picture} alt={data.fullName} />
                             <div className={styles.textContainer}>
                                 <p>{data.comment}</p>
