5cb88acfc46e6ac4743303a178c0accc2999a55a
Mehdi Salimi <Salimi.Devop@gmail.com>
2022-05-23 05:46:27 +0430

    build:Add blog data

---
+++ src/data/blogData.js
@@ -0,0 +1,258 @@
+//Blog images
+import blog1 from '../assets/images/blog-1.jpg';
+import blog2 from '../assets/images/blog-2.jpg';
+import blog3 from '../assets/images/blog-3.jpg';
+import blog4 from '../assets/images/blog-4.jpg';
+import blog5 from '../assets/images/blog-5.jpg';
+import blog6 from '../assets/images/blog-6.jpg';
+import blog7 from '../assets/images/blog-7.jpg';
+import blog8 from '../assets/images/blog-8.jpg';
+import blog9 from '../assets/images/blog-9.jpg';
+import blog10 from '../assets/images/blog-10.jpg';
+import blog11 from '../assets/images/blog-11.pg';
+
+//Blog data
+export const blogData = [
+    {
+        title: 'Golden History Museum',
+        second_title: 'Colorado School of Mines',
+        description:
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.',
+        text_content_1:
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived.',
+        text_content_2:
+            'Sed pharetra feugiat lacinia. Suspendisse rhoncus nunc diam, sit amet consectetur elit elementum sit amet. Sed cursus justo in ornare tristique. Nulla facilisi. Aliquam tincidunt tellus ut eros pellentesque laoreet. Sed sed lectus et massa scelerisque maximus. Duis tempus sem et nunc egestas, ut malesuada enim maximus.',
+        text_content_3:
+            'Fusce sit amet hendrerit ligula. Ut molestie metus turpis, id pellentesque tortor consequat nec. Etiam a hendrerit elit, non vulputate lorem.',
+        image: blog1,
+        items: [
+            'Sed ac rhoncus ipsum.',
+            'Praesent euismod purus at lacus ultrice.',
+            'Hole in the Rock Cruise',
+            'Maecenas pretium magna et lorem laoreet, eget fringilla ipsum pharetra.',
+        ],
+        author: 'admin',
+        category: ['Golf', 'Park'],
+        tag: ['golf', 'park'],
+    },
+    {
+        title: 'Foothills Art Center',
+        second_title: 'Colorado School of Mines',
+        description:
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.',
+        text_content_1:
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived.',
+        text_content_2:
+            'Sed pharetra feugiat lacinia. Suspendisse rhoncus nunc diam, sit amet consectetur elit elementum sit amet. Sed cursus justo in ornare tristique. Nulla facilisi. Aliquam tincidunt tellus ut eros pellentesque laoreet. Sed sed lectus et massa scelerisque maximus. Duis tempus sem et nunc egestas, ut malesuada enim maximus.',
+        text_content_3:
+            'Fusce sit amet hendrerit ligula. Ut molestie metus turpis, id pellentesque tortor consequat nec. Etiam a hendrerit elit, non vulputate lorem.',
+        image: blog2,
+        items: [
+            'Sed ac rhoncus ipsum.',
+            'Praesent euismod purus at lacus ultrice.',
+            'Hole in the Rock Cruise',
+            'Maecenas pretium magna et lorem laoreet, eget fringilla ipsum pharetra.',
+        ],
+        author: 'admin',
+        category: ['Mountain', 'Museum'],
+        tag: [],
+    },
+    {
+        title: 'Waitangi Treaty Grounds',
+        second_title: 'Colorado School of Mines',
+        description:
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.',
+        text_content_1:
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived.',
+        text_content_2:
+            'Sed pharetra feugiat lacinia. Suspendisse rhoncus nunc diam, sit amet consectetur elit elementum sit amet. Sed cursus justo in ornare tristique. Nulla facilisi. Aliquam tincidunt tellus ut eros pellentesque laoreet. Sed sed lectus et massa scelerisque maximus. Duis tempus sem et nunc egestas, ut malesuada enim maximus.',
+        text_content_3:
+            'Fusce sit amet hendrerit ligula. Ut molestie metus turpis, id pellentesque tortor consequat nec. Etiam a hendrerit elit, non vulputate lorem.',
+        image: blog3,
+        items: [
+            'Sed ac rhoncus ipsum.',
+            'Praesent euismod purus at lacus ultrice.',
+            'Hole in the Rock Cruise',
+            'Maecenas pretium magna et lorem laoreet, eget fringilla ipsum pharetra.',
+        ],
+        author: 'admin',
+        category: ['Museum', 'Park'],
+        tag: [],
+    },
+    {
+        title: 'MillerCoors Brewery',
+        second_title: 'Colorado School of Mines',
+        description:
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.',
+        text_content_1:
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived.',
+        text_content_2:
+            'Sed pharetra feugiat lacinia. Suspendisse rhoncus nunc diam, sit amet consectetur elit elementum sit amet. Sed cursus justo in ornare tristique. Nulla facilisi. Aliquam tincidunt tellus ut eros pellentesque laoreet. Sed sed lectus et massa scelerisque maximus. Duis tempus sem et nunc egestas, ut malesuada enim maximus.',
+        text_content_3:
+            'Fusce sit amet hendrerit ligula. Ut molestie metus turpis, id pellentesque tortor consequat nec. Etiam a hendrerit elit, non vulputate lorem.',
+        image: blog4,
+        items: [
+            'Sed ac rhoncus ipsum.',
+            'Praesent euismod purus at lacus ultrice.',
+            'Hole in the Rock Cruise',
+            'Maecenas pretium magna et lorem laoreet, eget fringilla ipsum pharetra.',
+        ],
+        author: 'admin',
+        category: ['Golf', 'Park'],
+        tag: ['fishing', 'park'],
+    },
+    {
+        title: 'Golden Gate Canyon State Park',
+        second_title: 'Colorado School of Mines',
+        description:
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.',
+        text_content_1:
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived.',
+        text_content_2:
+            'Sed pharetra feugiat lacinia. Suspendisse rhoncus nunc diam, sit amet consectetur elit elementum sit amet. Sed cursus justo in ornare tristique. Nulla facilisi. Aliquam tincidunt tellus ut eros pellentesque laoreet. Sed sed lectus et massa scelerisque maximus. Duis tempus sem et nunc egestas, ut malesuada enim maximus.',
+        text_content_3:
+            'Fusce sit amet hendrerit ligula. Ut molestie metus turpis, id pellentesque tortor consequat nec. Etiam a hendrerit elit, non vulputate lorem.',
+        image: blog5,
+        items: [
+            'Sed ac rhoncus ipsum.',
+            'Praesent euismod purus at lacus ultrice.',
+            'Hole in the Rock Cruise',
+            'Maecenas pretium magna et lorem laoreet, eget fringilla ipsum pharetra.',
+        ],
+        author: 'admin',
+        category: ['Mountain', 'Park'],
+        tag: ['mountain', 'park'],
+    },
+    {
+        title: 'Waitangi Golf Club',
+        second_title: 'Colorado School of Mines',
+        description:
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.',
+        text_content_1:
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived.',
+        text_content_2:
+            'Sed pharetra feugiat lacinia. Suspendisse rhoncus nunc diam, sit amet consectetur elit elementum sit amet. Sed cursus justo in ornare tristique. Nulla facilisi. Aliquam tincidunt tellus ut eros pellentesque laoreet. Sed sed lectus et massa scelerisque maximus. Duis tempus sem et nunc egestas, ut malesuada enim maximus.',
+        text_content_3:
+            'Fusce sit amet hendrerit ligula. Ut molestie metus turpis, id pellentesque tortor consequat nec. Etiam a hendrerit elit, non vulputate lorem.',
+        image: blog6,
+        items: [
+            'Sed ac rhoncus ipsum.',
+            'Praesent euismod purus at lacus ultrice.',
+            'Hole in the Rock Cruise',
+            'Maecenas pretium magna et lorem laoreet, eget fringilla ipsum pharetra.',
+        ],
+        author: 'admin',
+        category: ['Golf'],
+        tag: ['golf'],
+    },
+    {
+        title: 'Clear Creek History Park',
+        second_title: 'Colorado School of Mines',
+        description:
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.',
+        text_content_1:
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived.',
+        text_content_2:
+            'Sed pharetra feugiat lacinia. Suspendisse rhoncus nunc diam, sit amet consectetur elit elementum sit amet. Sed cursus justo in ornare tristique. Nulla facilisi. Aliquam tincidunt tellus ut eros pellentesque laoreet. Sed sed lectus et massa scelerisque maximus. Duis tempus sem et nunc egestas, ut malesuada enim maximus.',
+        text_content_3:
+            'Fusce sit amet hendrerit ligula. Ut molestie metus turpis, id pellentesque tortor consequat nec. Etiam a hendrerit elit, non vulputate lorem.',
+        image: blog7,
+        items: [
+            'Sed ac rhoncus ipsum.',
+            'Praesent euismod purus at lacus ultrice.',
+            'Hole in the Rock Cruise',
+            'Maecenas pretium magna et lorem laoreet, eget fringilla ipsum pharetra.',
+        ],
+        author: 'admin',
+        category: ['Museum', 'Park'],
+        tag: ['fishing', 'park'],
+    },
+    {
+        title: 'Astor House Museum',
+        second_title: 'Colorado School of Mines',
+        description:
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.',
+        text_content_1:
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived.',
+        text_content_2:
+            'Sed pharetra feugiat lacinia. Suspendisse rhoncus nunc diam, sit amet consectetur elit elementum sit amet. Sed cursus justo in ornare tristique. Nulla facilisi. Aliquam tincidunt tellus ut eros pellentesque laoreet. Sed sed lectus et massa scelerisque maximus. Duis tempus sem et nunc egestas, ut malesuada enim maximus.',
+        text_content_3:
+            'Fusce sit amet hendrerit ligula. Ut molestie metus turpis, id pellentesque tortor consequat nec. Etiam a hendrerit elit, non vulputate lorem.',
+        image: blog8,
+        items: [
+            'Sed ac rhoncus ipsum.',
+            'Praesent euismod purus at lacus ultrice.',
+            'Hole in the Rock Cruise',
+            'Maecenas pretium magna et lorem laoreet, eget fringilla ipsum pharetra.',
+        ],
+        author: 'admin',
+        category: ['Golf', 'Park'],
+        tag: ['golf', 'park'],
+    },
+    {
+        title: 'Swim with the Dolphins',
+        second_title: 'Colorado School of Mines',
+        description:
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.',
+        text_content_1:
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived.',
+        text_content_2:
+            'Sed pharetra feugiat lacinia. Suspendisse rhoncus nunc diam, sit amet consectetur elit elementum sit amet. Sed cursus justo in ornare tristique. Nulla facilisi. Aliquam tincidunt tellus ut eros pellentesque laoreet. Sed sed lectus et massa scelerisque maximus. Duis tempus sem et nunc egestas, ut malesuada enim maximus.',
+        text_content_3:
+            'Fusce sit amet hendrerit ligula. Ut molestie metus turpis, id pellentesque tortor consequat nec. Etiam a hendrerit elit, non vulputate lorem.',
+        image: blog9,
+        items: [
+            'Sed ac rhoncus ipsum.',
+            'Praesent euismod purus at lacus ultrice.',
+            'Hole in the Rock Cruise',
+            'Maecenas pretium magna et lorem laoreet, eget fringilla ipsum pharetra.',
+        ],
+        author: 'admin',
+        category: ['Mountain', 'Museum'],
+        tag: [],
+    },
+    {
+        title: 'Hole in the Rock Cruise',
+        second_title: 'Colorado School of Mines',
+        description:
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.',
+        text_content_1:
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived.',
+        text_content_2:
+            'Sed pharetra feugiat lacinia. Suspendisse rhoncus nunc diam, sit amet consectetur elit elementum sit amet. Sed cursus justo in ornare tristique. Nulla facilisi. Aliquam tincidunt tellus ut eros pellentesque laoreet. Sed sed lectus et massa scelerisque maximus. Duis tempus sem et nunc egestas, ut malesuada enim maximus.',
+        text_content_3:
+            'Fusce sit amet hendrerit ligula. Ut molestie metus turpis, id pellentesque tortor consequat nec. Etiam a hendrerit elit, non vulputate lorem.',
+        image: blog10,
+        items: [
+            'Sed ac rhoncus ipsum.',
+            'Praesent euismod purus at lacus ultrice.',
+            'Hole in the Rock Cruise',
+            'Maecenas pretium magna et lorem laoreet, eget fringilla ipsum pharetra.',
+        ],
+        author: 'admin',
+        category: ['Golf', 'Museum'],
+        tag: [],
+    },
+    {
+        title: 'American Mountaineering Center',
+        second_title: 'Colorado School of Mines',
+        description:
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.',
+        text_content_1:
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived.',
+        text_content_2:
+            'Sed pharetra feugiat lacinia. Suspendisse rhoncus nunc diam, sit amet consectetur elit elementum sit amet. Sed cursus justo in ornare tristique. Nulla facilisi. Aliquam tincidunt tellus ut eros pellentesque laoreet. Sed sed lectus et massa scelerisque maximus. Duis tempus sem et nunc egestas, ut malesuada enim maximus.',
+        text_content_3:
+            'Fusce sit amet hendrerit ligula. Ut molestie metus turpis, id pellentesque tortor consequat nec. Etiam a hendrerit elit, non vulputate lorem.',
+        image: blog11,
+        items: [
+            'Sed ac rhoncus ipsum.',
+            'Praesent euismod purus at lacus ultrice.',
+            'Hole in the Rock Cruise',
+            'Maecenas pretium magna et lorem laoreet, eget fringilla ipsum pharetra.',
+        ],
+        author: 'admin',
+        category: ['Mountain', 'Park'],
+        tag: [],
+    },
+];
