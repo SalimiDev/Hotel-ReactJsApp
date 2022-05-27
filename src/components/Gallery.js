930dff1fb95c037ba27d7e99b147fedc272a9bb1
Mehdi Salimi <Salimi.Devop@gmail.com>
2022-05-27 23:57:52 +0430

    feat:Add functionality to gallery for filtering result by tab menu name

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,22 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from '../styles/layout/Gallery.module.scss';
 import { room, blog } from '../assets/images';
 
 const Gallery = () => {
+    const [gallery, setGallery] = useState([]);
     const roomList = Object.values(room);
     const blogList = Object.values(blog);
-    const gallery = [...roomList, ...blogList];
+
+    //Filtering result
+    const filterGallery = menuTab => {
+        switch (menuTab) {
+            case 'bathroom':
+                setGallery([...roomList]);
+                break;
+            case 'dining':
+                setGallery([...blogList]);
+                break;
+            case 'hotel':
+                setGallery([...roomList]);
+                break;
+            case 'room':
+                setGallery([...roomList]);
+                break;
+            default:
+                setGallery([...roomList, ...blogList]);
+        }
+    };
+
+    //Set default gallery value
+    useEffect(() => {
+        setGallery([...roomList, ...blogList]);
+    }, []);
 
     return (
         <section className={styles.gallery}>
             <div className={styles.container}>
                 <h2>GALLERY</h2>
                 <ul className={styles.galleryMenu}>
-                    <li>all</li>
-                    <li>bathroom</li>
-                    <li>dining</li>
-                    <li>hotel & ground</li>
-                    <li>room & suite</li>
+                    <li onClick={() => filterGallery('all')}>all</li>
+                    <li onClick={() => filterGallery('bathroom')}>bathroom</li>
+                    <li onClick={() => filterGallery('dining')}>dining</li>
+                    <li onClick={() => filterGallery('hotel')}>hotel & ground</li>
+                    <li onClick={() => filterGallery('room')}>room & suite</li>
                 </ul>
                 <figure className={styles.gallery_container}>
                     {gallery.slice(0, 12).map((item, index) => (
