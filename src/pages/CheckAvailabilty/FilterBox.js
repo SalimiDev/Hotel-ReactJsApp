abe3692d8c1f715bdb48d352ff541a222a49474b
Mehdi Salimi <Salimi.devop@gmail.com>
2022-09-10 15:28:03 +0430

    feat:Add sorting  functionality & set default on render in FilterBox

--- src/pages/CheckAvailabilty/FilterBox.js
+++ src/pages/CheckAvailabilty/FilterBox.js
@@ -1,14 +1,34 @@
-import React from 'react';
-//styles
-import { FilterBoxContainer } from './CheckAvailibility.styles';
+import React, { useEffect } from 'react';
 import ActiveLink from '../../helpers/ActiveLink';
 import { useSearchParams } from 'react-router-dom';
+//styles
+import { FilterBoxContainer } from './CheckAvailibility.styles';
 
 const FilterBox = () => {
     const [searchParams, setSearchParams] = useSearchParams();
     let params = Object.fromEntries([...searchParams]);
+    const { sortby } = params;
 
-    const showPrice = e => {
+    //set default values on render
+    useEffect(() => {
+        setSearchParams({
+            ...params,
+            sortby: sortby || 'cheapest',
+            showprice: 'pernight',
+        });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    //handle to set sorting by show prices as highest price or cheapest price
+    const handleSortbyPrice = e => {
+        setSearchParams({
+            ...params,
+            sortby: e.target.value,
+        });
+    };
+
+    //handle to show total prices or prices per night
+    const showAmountPrice = e => {
         e.preventDefault();
         setSearchParams({
             ...params,
@@ -21,21 +41,21 @@ const FilterBox = () => {
             <div className='filterbox_box'>
                 <div className='filterbox_box-wrap'>
                     <label className='filterbox_label'>Sort by:</label>
-                    <select name='sortby' className='filterbox_sortby'>
-                        <option value=''>Cheapest price first</option>
-                        <option value=''>Highest price first</option>
+                    <select name='sortby' className='filterbox_sortby' onChange={handleSortbyPrice} defaultValue={sortby}>
+                        <option value='cheapest'>Cheapest price first</option>
+                        <option value='highest'>Highest price first</option>
                     </select>
                 </div>
                 <div className='filterbox_box-wrap'>
                     <label className='filterbox_label'>Show price:</label>
                     <ul className='filterbox_showprice'>
                         <li className='filterbox_showprice-item'>
-                            <ActiveLink name='pernight' onClick={showPrice} to={'/'} color='#888'>
+                            <ActiveLink name='pernight' onClick={showAmountPrice} to={'/'} color='#888'>
                                 Per room per night
                             </ActiveLink>
                         </li>
                         <li className='filterbox_showprice-item'>
-                            <ActiveLink name='total' onClick={showPrice} to={'/'} color='#888'>
+                            <ActiveLink name='total' onClick={showAmountPrice} to={'/'} color='#888'>
                                 Price for whole stay
                             </ActiveLink>
                         </li>
